7f886ed8a12b99cdfe4d650ac0d2f2fbf45e7c60
claud <claudio104.as@gmail.com>
2026-08-24 18:28:15 -0600

    Agregando validacion en contraseña y usuario

--- AccesoSistemaAcademico/src/main/java/ni/uam/edu/accesosistemaacademico/HelloApplication.java
+++ AccesoSistemaAcademico/src/main/java/ni/uam/edu/accesosistemaacademico/HelloApplication.java
@@ -1,8 +1,12 @@
 package ni.uam.edu.accesosistemaacademico;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,8 +15,8 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("FACEBOOK");
         stage.setScene(scene);
         stage.show();
     }
--- AccesoSistemaAcademico/src/main/java/ni/uam/edu/accesosistemaacademico/HelloController.java
+++ AccesoSistemaAcademico/src/main/java/ni/uam/edu/accesosistemaacademico/HelloController.java
@@ -2,13 +2,54 @@ package ni.uam.edu.accesosistemaacademico;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 public class HelloController {
     @FXML
+    private Label lblMiEtiqueta;
+    @FXML
+    public void initialize() {
+        Image miImagen = new Image(getClass().getResourceAsStream("labels.jpg"));
+
+        ImageView imageView = new ImageView(miImagen);
+        imageView.setFitWidth(50);
+        imageView.setFitHeight(50);
+        imageView.setPreserveRatio(true);
+
+        lblMiEtiqueta.setGraphic(imageView);
+    }
+    @FXML
     private Label welcomeText;
+    @FXML private TextField txtUsuario;
+    @FXML private PasswordField txtClave;
+    @FXML private Label lblMensaje;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void btnConfirmar() {
+        String usuario = txtUsuario.getText().trim();
+        String clave = txtClave.getText();
+
+        if (usuario.isEmpty() || clave.isEmpty()) {
+            lblMensaje.setText("Complete todos los campos.");
+            return;
+        }
+        if (usuario.equals("admin") && clave.equals("admin")){
+            lblMensaje.setText("Datos recibidos para validación.");
+            txtUsuario.clear();
+            txtClave.clear();
+        }
+        else{
+            lblMensaje.setText("Usuario o contraseña incorrectos.");
+            txtUsuario.clear();
+            txtClave.clear();
+        }
     }
+
 }
