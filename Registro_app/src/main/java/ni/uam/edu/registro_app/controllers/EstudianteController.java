f1bdda34b9a699180946763d1f166803478e5caa
Geanrm30 <garodriguezm@uamv.edu.ni>
2026-08-28 16:19:30 -0600

    Agregando TableView

--- Registro_app/src/main/java/ni/uam/edu/registro_app/controllers/EstudianteController.java
+++ Registro_app/src/main/java/ni/uam/edu/registro_app/controllers/EstudianteController.java
@@ -1,5 +1,8 @@
 package ni.uam.edu.registro_app.controllers;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import ni.uam.edu.registro_app.DAO.EstudianteDAO;
@@ -20,7 +23,16 @@ public class EstudianteController {
     @FXML private RadioButton rdMujer;
     @FXML private ComboBox<String> cmbCiudad;
     @FXML private Label lblMensajeError;
+    @FXML private TableView<Estudiante> tblEstudiantes;
+    @FXML private TableColumn<Estudiante, String> colNombre;
+    @FXML private TableColumn<Estudiante, String> colApellidos;
+    @FXML private TableColumn<Estudiante, String> colCarrera;
+    @FXML private TableColumn<Estudiante, String> colFechaNac;
+    @FXML private TableColumn<Estudiante, String> colCiudad;
+    @FXML private TableColumn<Estudiante, String> colSexo;
+    @FXML private TableColumn<Estudiante, String> colBeca;
     private ToggleGroup rdSexo;
+    private final ObservableList<Estudiante> datosTabla = FXCollections.observableArrayList();
 
     @FXML
     public void initialize() {
@@ -29,7 +41,29 @@ public class EstudianteController {
         rdSexo = new ToggleGroup();
         rdMujer.setToggleGroup(rdSexo);
         rdHombre.setToggleGroup(rdSexo);
+        prepararTabla();
     }
+
+    private void prepararTabla() {
+        colNombre.setCellValueFactory(fila -> new SimpleStringProperty(fila.getValue().getNombre()));
+        colApellidos.setCellValueFactory(fila -> new SimpleStringProperty(fila.getValue().getApellido()));
+        colCarrera.setCellValueFactory(fila -> new SimpleStringProperty(fila.getValue().getCarrera()));
+        colFechaNac.setCellValueFactory(fila -> {
+            LocalDate fecha = fila.getValue().getFechaNacimiento();
+            return new SimpleStringProperty(fecha == null ? "" : fecha.toString());
+        });
+        colCiudad.setCellValueFactory(fila -> new SimpleStringProperty(fila.getValue().getHabitacion()));
+        colSexo.setCellValueFactory(fila -> {
+            RadioButton sexo = fila.getValue().getSexo();
+            return new SimpleStringProperty(sexo == null ? "" : sexo.getText());
+        });
+        colBeca.setCellValueFactory(fila -> {
+            Boolean beca = fila.getValue().getTieneBeca();
+            return new SimpleStringProperty(Boolean.TRUE.equals(beca) ? "Si" : "No");
+        });
+        tblEstudiantes.setItems(datosTabla);
+    }
+
     @FXML protected void guardarOnClick(){
         leerDatos();
         contrarRegistro();
@@ -61,6 +95,7 @@ public class EstudianteController {
     }
     private void agregarDatos(Estudiante estudiante){
         listado.agregar(estudiante);
+        datosTabla.setAll(listado.obteneRegistro());
         cleanView();
     }
     private void contrarRegistro(){
