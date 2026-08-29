9133e46dff79307370e7a9f20ae5effe047b3fde
claud <claudio104.as@gmail.com>
2026-08-28 19:09:05 -0600

    Boton de confirmar por defecto al presionar enter

--- Registro_app/src/main/java/ni/uam/edu/registro_app/controllers/LoginController.java
+++ Registro_app/src/main/java/ni/uam/edu/registro_app/controllers/LoginController.java
@@ -3,6 +3,7 @@ package ni.uam.edu.registro_app.controllers;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
@@ -17,11 +18,19 @@ public class LoginController {
     @FXML private TextField txtUsuario;
     @FXML private PasswordField txtClave;
     @FXML private Label lblMensaje;
+    @FXML private Button confirmarBoton;
+
+    @FXML public void initialize(){
+        confirmarBoton.setDefaultButton(true);
+    }
+
 
     @FXML
     protected void btnConfirmar() {
         String usuario = txtUsuario.getText().trim();
         String clave = txtClave.getText();
+
+
 
         if (usuario.isEmpty() || clave.isEmpty()) {
             lblMensaje.setText("Complete todos los campos.");
